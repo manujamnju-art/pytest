0ce560e9db8eb8083cbee6e651b655720ebb1494
manujamnju-art <manujamnju@gmail.com>
2025-12-01 17:21:16 +0530

    Update test_employe.py

--- test_employe.py
+++ test_employe.py
@@ -1,9 +1,9 @@
-from employe import employe_info
-def test_employe_info():
+from employe import employee_info
+def test_employee_info():
    expected_output = (
       "Employee Name:Alice\n"
       "Employee ID:E123\n"
       "Department:IT\n"
       "Salary:50000"
     )
-   assert employe_info("Alice", "E1001", "IT", 55000) == expected_output
+   assert employee_info("Alice", "E1001", "IT", 55000) == expected_output
