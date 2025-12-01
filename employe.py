3ad1ba4fa9fa764a3dc915da514b4d4fa3d6a2a6
manujamnju-art <manujamnju@gmail.com>
2025-12-01 17:06:11 +0530

    Update employe.py

--- employe.py
+++ employe.py
@@ -1,19 +1,15 @@
-
 def employee_info(name, emp_id, department, salary):
     result = (
-        f"Employee Name: {name}\n"
-        f"Employee ID: {emp_id}\n"
-        f"Department: {department}\n"
-        f"Salary: {salary}"
+        f"Employee Name:{name}\n"
+        f"Employee ID:{emp_id}\n"
+        f"Department:{department}\n"
+        f"Salary:{salary}"
     )
     return result
-
 if __name__ == "__main__":
-   
-        name = "Alice\n"
-        emp_id = "E1001\n"
-        department = "IT\n"
-        salary = 55000
-
-        print(employee_info(name, emp_id, department, salary))
+    name = "Alice\n"
+    emp_id = "E1001\n"
+    department = "IT\n"
+    salary = 55000
+    print(employee_info(name, emp_id, department, salary))
     
