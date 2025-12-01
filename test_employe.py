968995471094dd76c3950d9d31b21bf3ebac213b
manujamnju-art <manujamnju@gmail.com>
2025-12-01 17:22:34 +0530

    Update and rename test_employe.py to test_employee.py

--- test_employe.py
+++
@@ -1,9 +0,0 @@
-from employe import employee_info
-def test_employee_info():
-   expected_output = (
-      "Employee Name:Alice\n"
-      "Employee ID:E123\n"
-      "Department:IT\n"
-      "Salary:50000"
-    )
-   assert employee_info("Alice", "E1001", "IT", 55000) == expected_output
---
+++ test_employee.py
@@ -0,0 +1,9 @@
+from employe import employee_info
+def test_employee_info():
+   expected_output =(
+      "Employee_Name:Alice\n"
+      "Employee_ID:E123\n"
+      "Department:IT\n"
+      "Salary:50000"
+    )
+   assert employee_info("Alice","E1001","IT",55000) == expected_output
